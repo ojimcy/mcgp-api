c12de335706559a8dedf4a28d7e411c0efbeb5e4
Emmanuel <ojimcy247@gmail.com>
2024-06-25 15:52:15 +0100

    update multer

--- src/config/multer.js
+++ src/config/multer.js
@@ -2,17 +2,8 @@ const multer = require('multer');
 const path = require('path');
 
 // Multer config
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
-  },
-});
-
-const upload = multer({
-  storage,
+module.exports = multer({
+  storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     const ext = path.extname(file.originalname);
     if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
@@ -22,5 +13,3 @@ const upload = multer({
     cb(null, true);
   },
 });
-
-module.exports = upload;
